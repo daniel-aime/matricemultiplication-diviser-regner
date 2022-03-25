4b737405b9338fbee440b8f9318d1a525b3efc5d
daniel <rastaitrbeatz320@gmail.com>
2022-03-25 19:54:43 +0300

    On progress 2

--- matrix_multiplication.py
+++ matrix_multiplication.py
@@ -5,36 +5,36 @@ ntype = np.int64
 
 class matriceMultplication:
 
-  # def __init__(self, matrice_a_arr, matrice_b_arr) -> None:
-  #     self.matrice_a_arr = matrice_a_arr
-  #     self.matrice_b_arr = matrice_b_arr 
+  # def __init__(self, matrice_a, matrice_b) -> None:
+  #     self.matrice_a = matrice_a
+  #     self.matrice_b = matrice_b 
 
 
-   def matrice_multiply_old_methode(self, matrice_a_arr, matrice_b_arr):
-        n = matrice_a_arr.shape[0]
-        matrice_c_arr = np.zeros((n, n))
-        matrice_c_arr = matrice_c_arr.astype(ntype)
+   def matrice_multiply_old_methode(self, matrice_a, matrice_b):
+        n = matrice_a.shape[0]
+        matrice_c = np.zeros((n, n))
+        matrice_c = matrice_c.astype(ntype)
         for i in range(n):
             for j in range(n):
-                matrice_c_arr[i, j] = 0
+                matrice_c[i, j] = 0
                 for k in range(n):
-                    matrice_c_arr[i, j] += matrice_a_arr[i, k] * matrice_b_arr[k, j]
-        return matrice_c_arr
+                    matrice_c[i, j] += matrice_a[i, k] * matrice_b[k, j]
+        return matrice_c
   
   
-   def matrice_multiply_recursive_diviser_regner(self, matrice_a_arr, matrice_b_arr):
-        n = matrice_a_arr.shape[0]
-        matrice_c_arr = np.zeros((n, n))
-        matrice_c_arr = matrice_c_arr.astype(ntype)
+   def matrice_multiply_recursive_diviser_regner(self, matrice_a, matrice_b):
+        n = matrice_a.shape[0]
+        matrice_c = np.zeros((n, n))
+        matrice_c = matrice_c.astype(ntype)
         if n == 1:
-            matrice_c_arr[0, 0] = matrice_a_arr[0, 0] * matrice_b_arr[0, 0]
+            matrice_c[0, 0] = matrice_a[0, 0] * matrice_b[0, 0]
         else:
             demi_n = int(n / 2)
-            matrice_c_arr[0 : demi_n, 0 : demi_n] = self.matrice_multiply_recursive_diviser_regner(matrice_a_arr[0 : demi_n, 0 : demi_n], matrice_b_arr[0 : demi_n, 0 : demi_n]) + self.matrice_multiply_recursive_diviser_regner(matrice_a_arr[0 : demi_n, demi_n : n], matrice_b_arr[demi_n : n, 0 : demi_n])
-            matrice_c_arr[0 : demi_n, demi_n : n] = self.matrice_multiply_recursive_diviser_regner(matrice_a_arr[0 : demi_n, 0 : demi_n], matrice_b_arr[0 : demi_n, demi_n : n]) + self.matrice_multiply_recursive_diviser_regner(matrice_a_arr[0 : demi_n, demi_n : n], matrice_b_arr[demi_n : n, demi_n : n])
-            matrice_c_arr[demi_n : n, 0 : demi_n] = self.matrice_multiply_recursive_diviser_regner(matrice_a_arr[demi_n : n, 0 : demi_n], matrice_b_arr[0 : demi_n, 0 : demi_n]) + self.matrice_multiply_recursive_diviser_regner(matrice_a_arr[demi_n : n, demi_n : n], matrice_b_arr[demi_n : n, 0 : demi_n])
-            matrice_c_arr[demi_n : n, demi_n : n] = self.matrice_multiply_recursive_diviser_regner(matrice_a_arr[demi_n : n, 0 : demi_n], matrice_b_arr[0 : demi_n, demi_n : n]) + self.matrice_multiply_recursive_diviser_regner(matrice_a_arr[demi_n : n, demi_n : n], matrice_b_arr[demi_n : n, demi_n : n])
-        return matrice_c_arr
+            matrice_c[0 : demi_n, 0 : demi_n] = self.matrice_multiply_recursive_diviser_regner(matrice_a[0 : demi_n, 0 : demi_n], matrice_b[0 : demi_n, 0 : demi_n]) + self.matrice_multiply_recursive_diviser_regner(matrice_a[0 : demi_n, demi_n : n], matrice_b[demi_n : n, 0 : demi_n])
+            matrice_c[0 : demi_n, demi_n : n] = self.matrice_multiply_recursive_diviser_regner(matrice_a[0 : demi_n, 0 : demi_n], matrice_b[0 : demi_n, demi_n : n]) + self.matrice_multiply_recursive_diviser_regner(matrice_a[0 : demi_n, demi_n : n], matrice_b[demi_n : n, demi_n : n])
+            matrice_c[demi_n : n, 0 : demi_n] = self.matrice_multiply_recursive_diviser_regner(matrice_a[demi_n : n, 0 : demi_n], matrice_b[0 : demi_n, 0 : demi_n]) + self.matrice_multiply_recursive_diviser_regner(matrice_a[demi_n : n, demi_n : n], matrice_b[demi_n : n, 0 : demi_n])
+            matrice_c[demi_n : n, demi_n : n] = self.matrice_multiply_recursive_diviser_regner(matrice_a[demi_n : n, 0 : demi_n], matrice_b[0 : demi_n, demi_n : n]) + self.matrice_multiply_recursive_diviser_regner(matrice_a[demi_n : n, demi_n : n], matrice_b[demi_n : n, demi_n : n])
+        return matrice_c
 
 def main():
   matrice_a = np.random.randint(5, size=(16, 16))
