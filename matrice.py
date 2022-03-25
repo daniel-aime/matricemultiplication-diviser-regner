5a82eafa8700b2247599869949d783ed6d8a2a32
daniel <rastaitrbeatz320@gmail.com>
2022-03-25 08:58:58 +0300

    First commit

---
+++ matrice.py
@@ -0,0 +1,22 @@
+import random 
+
+# Set random the matrix A and B
+A = [ [random.randint(0, 10) for j in range(8)] for i in range(8) ]
+B = [ [random.randint(0, 10) for j in range(8)] for i in range(8) ]
+
+
+
+"""
+
+"""
+def fusion():
+  pass 
+
+
+def div(mat_A, mat_B):
+  pass 
+
+
+print(A)
+print(B)
+
